38623be4884b4f56cb5ffabcb71b31da8d8a175b
Jean-Marc M <jeanmarcmoeckel@gmail.com>
2020-06-02 16:59:22 +0200

    Add custom theme and global style settings

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,44 @@
 import React from "react"
 import { Container, Box } from "@material-ui/core"
+import { createMuiTheme } from "@material-ui/core/styles"
+import { ThemeProvider } from "@material-ui/styles"
+import CssBaseline from "@material-ui/core/CssBaseline"
 import Navbar from "../Navbar/Navbar"
 import Footer from "../Footer/Footer"
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: "Montserrat",
+    fontSize: 10,
+  },
+  palette: {
+    primary: {
+      main: "#002031",
+      light: "rgba(2,46,69,.68)",
+    },
+    secondary: {
+      main: "#5DD88D",
+    },
+    text: {
+      primary: "#F0F0F0",
+      secondary: "#D2D2D2",
+    },
+    background: {
+      default: "#002031",
+    },
+  },
+})
+
 const Layout = ({ children }) => {
   return (
-    <Container maxWidth="lg">
-      <Navbar />
-      <Box component="main">{children}</Box>
-      <Footer />
-    </Container>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Navbar />
+        <Box component="main">{children}</Box>
+        <Footer />
+      </Container>
+    </ThemeProvider>
   )
 }
 
