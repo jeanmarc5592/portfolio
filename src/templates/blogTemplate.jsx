ad7bee25d6b0b309504f9ea111911729ce01ce77
Jean-Marc M <jeanmarcmoeckel@gmail.com>
2020-06-08 21:33:24 +0200

    Fetch individual data for blog post

--- src/templates/blogTemplate.jsx
+++ src/templates/blogTemplate.jsx
@@ -1,7 +1,39 @@
 import React from "react"
+import { graphql } from "gatsby"
+import { Typography } from "@material-ui/core"
+import Layout from "../components/Layout/Layout"
 
-const blogTemplate = () => {
-  return <div>Hello From the Blog Template!</div>
+export const data = graphql`
+  query getSingleBlogpost($slug: String) {
+    blogPost: contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      image {
+        fluid {
+          ...GatsbyContentfulFluid
+        }
+      }
+      date(formatString: "MMMM Do, YYYY")
+      author
+      content {
+        content
+      }
+    }
+  }
+`
+
+const blogTemplate = ({ data }) => {
+  const {
+    title,
+    author,
+    date,
+    image: { fluid },
+    content: { content },
+  } = data.blogPost
+  return (
+    <Layout>
+      <Typography variant="h2">{title}</Typography>
+    </Layout>
+  )
 }
 
 export default blogTemplate
