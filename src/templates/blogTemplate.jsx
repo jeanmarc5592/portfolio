5ff32ac204ddd7de2c1f1345ff99b53fce018b71
Jean-Marc M <jeanmarcmoeckel@gmail.com>
2020-06-10 17:20:36 +0200

    Add Styling to blogTemplate.jsx

--- src/templates/blogTemplate.jsx
+++ src/templates/blogTemplate.jsx
@@ -2,8 +2,9 @@ import React from "react"
 import { graphql } from "gatsby"
 import Image from "gatsby-image"
 import RichText from "@madebyconnor/rich-text-to-jsx"
-import { Typography, Container } from "@material-ui/core"
+import { Typography, Container, Divider } from "@material-ui/core"
 import Layout from "../components/Layout/Layout"
+import CustomDivider from "../components/CustomDivider/CustomDivider"
 
 export const data = graphql`
   query getSingleBlogpost($slug: String) {
@@ -33,10 +34,38 @@ const blogTemplate = ({ data }) => {
   } = data.blogPost
   return (
     <Layout>
-      <Image fluid={fluid} style={{ maxHeight: "500px" }} />
+      <Image fluid={fluid} style={{ maxHeight: "500px", marginTop: "4rem" }} />
       <Container maxWidth="md">
-        <Typography variant="h2">{title}</Typography>
-        <RichText richText={json} />
+        <Typography
+          variant="h2"
+          align="left"
+          color="secondary"
+          style={{ fontWeight: 700, marginTop: "2rem" }}
+        >
+          {title}
+        </Typography>
+        <Typography
+          variant="body1"
+          component="p"
+          align="left"
+          style={{
+            marginBottom: "2rem",
+            marginLeft: "4px",
+            letterSpacing: "1px",
+            opacity: "0.85",
+          }}
+        >
+          {author} on {date}
+        </Typography>
+        <CustomDivider />
+        <Typography
+          variant="body1"
+          component="p"
+          align="left"
+          style={{ marginTop: "3rem", color: "#fff" }}
+        >
+          <RichText richText={json} />
+        </Typography>
       </Container>
     </Layout>
   )
